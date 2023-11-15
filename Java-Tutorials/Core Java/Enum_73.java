036f87adbecb2885e616314cba86d3ed6be63ea6
Suyash Mishra <suyash.mishra@vlinkinfo.com>
2023-11-15 22:31:20 +0530

    more enums

--- Java-Tutorials/Core Java/Enum_73.java
+++ Java-Tutorials/Core Java/Enum_73.java
@@ -1,47 +1,22 @@
-// enum Status {
-//   Running,
-//   Failed,
-//   Success,
-//   Pending,
-// }
+enum Status {
+  Running,
+  Failed,
+  Success,
+  Pending,
+}
 
-// public class Enum_73 {
+public class Enum_73 {
 
-//   public static void main(String args[]) {
-//     Status s = Status.Failed;
-//     System.out.println(s);
+  public static void main(String args[]) {
+    // ! Basic Enum
+    // Status s = Status.Failed;
+    // Status[] ss = Status.values();
+    // for (Status status : ss) {
+    //   System.out.println(status);
+    // }
 
-//   }
-// }
-
-
-// public class Enum_73 {
-
-//   public static String longestCommonPrefix(String[] strs) {
-//     Arrays.sort(strs);
-//     int length = strs.length;
-
-//     String firstStr = strs[0];
-//     String lastStr = strs[length - 1];
-//     StringBuilder commonPrefix = new StringBuilder();
-
-//     for (int i = 0; i < firstStr.length(); i++) {
-//         System.out.println(strs[i]);
-//       if (i < lastStr.length() && firstStr.charAt(i) == lastStr.charAt(i)) {
-//         commonPrefix.append(firstStr.charAt(i));
-//       } else {
-//         break;
-//       }
-//     }
-
-//     return commonPrefix.toString();
-//   }
-
-//   public static void main(String[] args) {
-//     String[] input = { "flower", "flow", "flight" };
-//     String output = longestCommonPrefix(input);
-
-//     System.out.println("Input: " + Arrays.toString(input));
-//     System.out.println("Output: " + output);
-//   }
-// }
+    // Status enum if and switch
+    Status s = Status.Pending;
+    
+  }
+}
