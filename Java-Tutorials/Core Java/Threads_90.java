f214746fcaee5bb0dfe6d1c761ee30f1c9c02e9f
Suyash Mishra <suyash.mishra@vlinkinfo.com>
2023-11-25 00:01:14 +0530

    threads

--- Java-Tutorials/Core Java/Threads_90.java
+++ Java-Tutorials/Core Java/Threads_90.java
@@ -3,6 +3,8 @@ public class Threads_90 extends Thread {
   // initiated run method for Thread
   public void run() {
     System.out.println("Thread Started Running...");
+
+    System.out.println("Thread Started Running...");
   }
 
   public static void main(String[] args) {
